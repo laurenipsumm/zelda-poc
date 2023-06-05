e04393881e18c1b5cbe54559fc5282ffa9f0c4f6
Lauren Conley <laurenashley.mail@gmail.com>
2023-06-05 16:04:16 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { PeerServer } = require("peer");
+
+const peerServer = PeerServer({
+  port: 9000, key:"demodemo", path: "/myapp",
+  allow_discovery: true,
+});
+
+
+peerServer.on("connection", c => {
+  console.log("connection ID:", c.id)
+})
+
+peerServer.on("disconnect", c => {
+  console.log("disconnect!", c.id)
+})
+
+console.log("running")
